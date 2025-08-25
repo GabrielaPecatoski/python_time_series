bed542713214602b0f9333219c669b55d7256e0b
Gabriela Pecatoski <152335722+GabrielaPecatoski@users.noreply.github.com>
2025-08-25 20:29:22 -0300

    Update cadeira_preto.py
    Aula 25-08

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,8 @@
-
+O que é um cabeçalho para Python dentro de um arquivo?
+R: É um bloco no início do código, geralmente com comentários ou docstrings, que identifica e descreve o arquivo.
+para que serve?
+R: Serve para documentar o propósito do script, indicar o interpretador e facilitar a leitura e manutenção do código.
+o que é uma docstring?
+R: São strings colocadas logo apos uma função, classe ou módulo e são usadas para documenta-las. São delimitadas por aspas triplas e diferem dos comentários, pois docstrings podem ser acessadas em tempo de execução, tornando-as úteis para gerar documentações automáticas e facilitar a compreensão do código.
+para que serve?
+R: Serve para documentar uma função, classe ou módulo dentro de python, tornando-as úteis para gerar documentações automáticas e facilitar a compreensão do código.
